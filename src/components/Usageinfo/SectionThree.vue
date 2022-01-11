a5744241533450aef8c2201d9fc5c157ec402093
Vishwa.R <v.i.s.h.w.a.08.11.2001@gmail.com>
2022-01-11 23:08:25 +0530

    style: add new component and assets #5
    - add SectionThree component for usageinfo page
    - add global CSS at Usageinfo.vue
    - add assets need for SectionThree component

---
+++ src/components/Usageinfo/SectionThree.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="main__wrapper">
+    <div class="main__container">
+      <p class="primary__heading">Installing using the .xpi file</p>
+      <p class="norm__text">
+        To install the add-on using the .xpi file, follow the below steps.
+      </p>
+      <ol class="list__group norm__text">
+        <li>
+          In this repository navigate to Firfox/onlyhtml-1.0-fx.xpi or click
+          <a href="#">[here]</a>.
+        </li>
+        <li>Click on the <strong>Download</strong> button.</li>
+        <img src="@/assets/image3.png" alt="download-button" />
+        <li>
+          Open <strong>Add-ons and Themes</strong> menu or press
+          <strong>Ctrl+Shift+A</strong>
+        </li>
+        <img src="@/assets/image4.png" alt="menu-screenshot" />
+        <li>
+          Click on gear icon and Install
+          <strong>Add-on from file...</strong> option inside menu.
+        </li>
+        <img
+          src="@/assets/image5.png"
+          alt="Add-on from file option in the menu"
+        />
+        <li>
+          Select the <strong>.xpi</strong> file, downloaded in
+          <strong>Step 2</strong>.
+        </li>
+        <li>
+          Click on <strong>Add</strong> and <strong>Okay</strong> in the
+          subsequent prompts.
+        </li>
+        <li>Now you can use the add-on normally.</li>
+      </ol>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SectionThree",
+};
+</script>
+
+<style scoped>
+/* for small screens */
+@media (max-width: 576px) {
+}
+/* for large screens */
+@media (min-width: 992px) {
+  .main__wrapper {
+    margin-top: 10rem;
+    width: 56.5rem;
+    padding: 1rem 3rem;
+  }
+  .list__group {
+    padding-left: 3rem;
+  }
+  .primary__heading {
+    font-size: 48pt;
+    padding-bottom: 2rem;
+    color: var(--ternary-text-color);
+  }
+  .norm__text {
+    font-size: 18pt;
+    color: var(--subtitle-text-color);
+  }
+  .subtitle__text {
+    font-size: 27pt;
+    padding-bottom: 2rem;
+    color: var(--button-bg-color);
+  }
+  img {
+    margin: 1rem 0rem 0rem -2.7rem;
+  }
+}
+</style>
--- src/views/Usageinfo.vue
+++ src/views/Usageinfo.vue
@@ -6,19 +6,31 @@
     <SectionOne />
     <!-- Section two component -->
     <SectionTwo />
+    <!-- Section three component -->
+    <SectionThree />
+    <!-- Footer component -->
+    <Footer class="footer__component" />
   </div>
 </template>
 <script>
 import Header from "@/components/Usageinfo/Header.vue";
 import SectionOne from "@/components/Usageinfo/SectionOne.vue";
 import SectionTwo from "@/components/Usageinfo/SectionTwo.vue";
+import SectionThree from "@/components/Usageinfo/SectionThree.vue";
+import Footer from "@/components/Homepage/Footer.vue";
 export default {
   name: "Usageinfo",
   components: {
     Header,
     SectionOne,
     SectionTwo,
+    SectionThree,
+    Footer,
   },
 };
 </script>
-<style scoped></style>
+<style scoped>
+.footer__component {
+  padding-bottom: 2rem;
+}
+</style>
